e35a8ab5a1d1d08e916b93073e3b8c65adb4fafb
Andy <andy.lee.surfer@gmail.com>
2017-02-13 15:51:59 +0000

    Removed settings for static maps

--- settings.js
+++ settings.js
@@ -1,17 +1,3 @@
 ﻿module.exports = {
-  port: 8080,
-  worlds: {
-    1: {
-      map: 'https://i1.wp.com/zwiftblog.com/wp-content/uploads/2016/03/watopia-1.10.png',
-      viewBox: '-551868 -323969 599071 900906'
-    },
-    2: {
-      map: 'https://i0.wp.com/zwiftblog.com/wp-content/uploads/2016/11/richmond-map-large.jpg',
-      viewBox: '-283211 -289039 691768 462822'
-    },
-    3: {
-      map: 'https://i0.wp.com/zwiftblog.com/wp-content/uploads/2016/08/London.gif',
-      viewBox: '114132 -204219 668391 444149'
-    }
-  }
+  port: 8080
 };

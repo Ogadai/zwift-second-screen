fbef54fd40d9af432c83b64ad33897fb270f6017
Andy <andy.lee.surfer@gmail.com>
2018-02-08 12:45:36 +0000

    Removed test code

--- server/pointsOfInterest.js
+++ server/pointsOfInterest.js
@@ -3,23 +3,23 @@ const NodeCache = require('node-cache')
 const poiCache = new NodeCache({ stdTTL: 30 * 60, checkPeriod: 120, useClones: false });
 
 // TODO: Remove
-let counter = 0;
-const flags = [false, false, false, false, false];
-setTimeout(() => {
-setInterval(() => {
-  if (counter < flags.length) {
-    flags[counter] = true;
-  }
+// let counter = 0;
+// const flags = [false, false, false, false, false];
+// setTimeout(() => {
+// setInterval(() => {
+//   if (counter < flags.length) {
+//     flags[counter] = true;
+//   }
 
-  if (counter === 0 || counter === 2) {
-//    flags[0] = false;
-    flags[4] = true;
-  } else if (counter === 1 || counter === 3) {
-    flags[4] = false;
-  }
-  counter++;
-}, 5000);
-}, 10000);
+//   if (counter === 0 || counter === 2) {
+// //    flags[0] = false;
+//     flags[4] = true;
+//   } else if (counter === 1 || counter === 3) {
+//     flags[4] = false;
+//   }
+//   counter++;
+// }, 5000);
+// }, 10000);
 
 class PointsOfInterest {
   constructor(worldSettings) {
@@ -32,10 +32,10 @@ class PointsOfInterest {
   }
 
   customiseForRider(worldId, points, positions) {
-//    const position = positions && positions.find(p => p.me);
+    const position = positions && positions.find(p => p.me);
 // TODO: Remove
-const position = { id: 101, x: 0, y: 0 };
-console.log(flags.join(','));
+// const position = { id: 101, x: 0, y: 0 };
+// console.log(flags.join(','));
     if (!position) {
       return points;
     }
@@ -57,9 +57,9 @@ console.log(flags.join(','));
           || (!riderPoints.find(p => !p.role && !p.visited));
 
       if (shouldCheck && !point.visited) {
-//        point.visited = this.checkVisited(position, cachedRider.positions, point);
+        point.visited = this.checkVisited(position, cachedRider.positions, point);
 // TODO: Remove
-point.visited = flags[index];
+// point.visited = flags[index];
       }
       riderPoints.push(point);
     });

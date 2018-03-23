cdb4fd58e2fff595f012c51d8d9ba03b9945fcf1
Andy <andy.lee.surfer@googlemail.com>
2018-03-23 19:49:45 +0000

    Removed rideon feature

--- src/components/rider-label.jsx
+++ src/components/rider-label.jsx
@@ -11,9 +11,7 @@ class RiderLabel extends Component {
     return {
       position: PropTypes.object.isRequired,
       scale: PropTypes.number,
-      useMetric: PropTypes.bool,
-      onRideOn: PropTypes.func,
-      sentRideOn: PropTypes.bool
+      useMetric: PropTypes.bool
     };
   }
 
@@ -24,10 +22,8 @@ class RiderLabel extends Component {
     const distFactor = useMetric ? 1 : M_TO_KM;
 
     return <g className="rider-label" transform={`translate(${(50000 - width / 2) / scale},${10000 / scale})`}>
-        <rect className="background" x="0" y="0" width={(30000 + width) / scale} height={30000 / scale}
+        <rect className="background" x="0" y="0" width={(width) / scale} height={30000 / scale}
             rx={5000 / scale} ry={5000 / scale} />
-
-        {this.renderRideOnButton()}
 
         {this.renderData(useMetric ? 'KM' : 'MILES', Math.round(distance / distFactor / 100) / 10, 20000)}
         {this.renderData('HRS', Math.round(time / (60*60)), 55000)}
@@ -50,23 +46,8 @@ class RiderLabel extends Component {
     };
 
     return <g>
-      <text className="label" x={30000 + offset} y={12000} {...props}>{label}</text>
-      <text className="value" x={30000 + offset} y={25000} {...props}>{value}</text>
-    </g>;
-  }
-
-  renderRideOnButton() {
-    const { scale, onRideOn, sentRideOn } = this.props;
-    const transform = `scale(${1/scale})`;
-
-    return <g>
-      {onRideOn && <rect className={classnames('rideOn', { disabled: sentRideOn })}
-          x={3000 / scale} y={3000 / scale} width={24000 / scale} height={24000 / scale}
-          rx={5000 / scale} ry={5000 / scale} onClick={() => onRideOn()} />}
-
-      {onRideOn && <image className="rideOn-thumb"
-          x={8000 / scale} y={8000 / scale} width={14000 / scale} height={14000 / scale}
-          xlinkHref="../../img/rideon.svg" />}
+      <text className="label" x={offset} y={12000} {...props}>{label}</text>
+      <text className="value" x={offset} y={25000} {...props}>{value}</text>
     </g>;
   }
 }
